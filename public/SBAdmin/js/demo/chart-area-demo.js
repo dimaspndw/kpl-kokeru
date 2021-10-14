1776bbcd211cbea17961aa994009dd2129f873a7
ahmadsakur <sakuretsuu@gmail.com>
2021-10-14 23:01:22 +0700

    Fix String Quote

--- public/SBAdmin/js/demo/chart-area-demo.js
+++ public/SBAdmin/js/demo/chart-area-demo.js
@@ -1,28 +1,28 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#858796';
+Chart.defaults.global.defaultFontColor = "#858796";
 
 function number_format(number, decimals, dec_point, thousands_sep) {
   // *     example: number_format(1234.56, 2, ',', ' ');
   // *     return: '1 234,56'
-  number = (number + '').replace(',', '').replace(' ', '');
+  number = (number + "").replace(",", "").replace(" ", "");
   var n = !isFinite(+number) ? 0 : +number,
     prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-    s = '',
+    sep = (typeof thousands_sep === "undefined") ? "," : thousands_sep,
+    dec = (typeof dec_point === "undefined") ? '.' : dec_point,
+    s = "",
     toFixedFix = function(n, prec) {
       var k = Math.pow(10, prec);
-      return '' + Math.round(n * k) / k;
+      return "" + Math.round(n * k) / k;
     };
   // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  s = (prec ? toFixedFix(n, prec) : "" + Math.round(n)).split(".");
   if (s[0].length > 3) {
     s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
   }
-  if ((s[1] || '').length < prec) {
-    s[1] = s[1] || '';
-    s[1] += new Array(prec - s[1].length + 1).join('0');
+  if ((s[1] || "").length < prec) {
+    s[1] = s[1] || "";
+    s[1] += new Array(prec - s[1].length + 1).join("0");
   }
   return s.join(dec);
 }
@@ -30,7 +30,7 @@ function number_format(number, decimals, dec_point, thousands_sep) {
 // Area Chart Example
 var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
-  type: 'line',
+  type: "line",
   data: {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [{
@@ -62,7 +62,7 @@ var myLineChart = new Chart(ctx, {
     scales: {
       xAxes: [{
         time: {
-          unit: 'date'
+          unit: "date"
         },
         gridLines: {
           display: false,
@@ -78,7 +78,7 @@ var myLineChart = new Chart(ctx, {
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            return "$" + number_format(value);
           }
         },
         gridLines: {
@@ -97,20 +97,20 @@ var myLineChart = new Chart(ctx, {
       backgroundColor: "rgb(255,255,255)",
       bodyFontColor: "#858796",
       titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
+      titleFontColor: "#6e707e",
       titleFontSize: 14,
-      borderColor: '#dddfeb',
+      borderColor: "#dddfeb",
       borderWidth: 1,
       xPadding: 15,
       yPadding: 15,
       displayColors: false,
       intersect: false,
-      mode: 'index',
+      mode: "index",
       caretPadding: 10,
       callbacks: {
         label: function(tooltipItem, chart) {
-          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || "";
+          return datasetLabel + ": $" + number_format(tooltipItem.yLabel);
         }
       }
     }
--- public/SBAdmin/js/sb-admin-2.js
+++ public/SBAdmin/js/sb-admin-2.js
@@ -6,7 +6,7 @@
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
     if ($(".sidebar").hasClass("toggled")) {
-      $('.sidebar .collapse').collapse('hide');
+      $(".sidebar .collapse").collapse('hide');
     };
   });
 
@@ -25,7 +25,7 @@
   });
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+  $("body.fixed-nav .sidebar").on("mousewheel DOMMouseScroll wheel", function(e) {
     if ($(window).width() > 768) {
       var e0 = e.originalEvent,
         delta = e0.wheelDelta || -e0.detail;
@@ -35,21 +35,21 @@
   });
 
   // Scroll to top button appear
-  $(document).on('scroll', function() {
+  $(document).on("scroll", function() {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
+      $(".scroll-to-top").fadeIn();
     } else {
-      $('.scroll-to-top').fadeOut();
+      $(".scroll-to-top").fadeOut();
     }
   });
 
   // Smooth scrolling using jQuery easing
-  $(document).on('click', 'a.scroll-to-top', function(e) {
+  $(document).on('click', "a.scroll-to-top", function(e) {
     var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top)
-    }, 1000, 'easeInOutExpo');
+    $("html, body").stop().animate({
+      scrollTop: ($($anchor.attr("href")).offset().top)
+    }, 1000, "easeInOutExpo");
     e.preventDefault();
   });
 
