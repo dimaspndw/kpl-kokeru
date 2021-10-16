15702e99a544fd4105ebc23ce3f357755175fded
dimaspndw <65652924+dimaspndw@users.noreply.github.com>
2021-10-16 22:12:03 +0700

    Delete show

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -56,17 +56,6 @@ class PegawaiController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    // public function show(User $user)
-    // {
-    //     //
-    // }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\User  $user
